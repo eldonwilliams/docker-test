1f8f64bc4fa20c993f7814740d84d4d3cfbacb49
Eldon Williams <eldon.ew.com@gmail.com>
2022-12-02 07:57:12 -0600

    test site prod

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('*', (req, res) => res.send('Hello, world!'));
+app.get('*', (req, res) => res.send(process.env));
 
 app.listen(3000, () => console.log("hi"));
